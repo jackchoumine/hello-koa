5623558d964926c452df0ab3fc3ac4f7d35c8fe7
jack <jackzhoumine@gmail.com>
2022-04-13 00:30:29 +0800

    路由中间件

--- index.js
+++ index.js
@@ -2,13 +2,21 @@
  * @Description : koa demo1
  * @Date        : 2022-04-12 00:07:53 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-04-12 23:30:28 +0800
+ * @LastEditTime: 2022-04-13 00:26:46 +0800
  * @LastEditors : JackChou
  */
 const Koa = require('koa')
 const app = new Koa()
 const { parsePostData } = require('./src/utils')
-// BUG koa 没有路由系统，只有中间件
+
+const Router = require('@koa/router')
+const router = new Router()
+
+router.get('/', (ctx, next) => {
+  // ctx.router available
+})
+
+app.use(router.routes()).use(router.allowedMethods())
 
 // ctx 是上下文
 app.use(async (ctx, next) => {
