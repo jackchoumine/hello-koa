ca77f9c22d68224cba4c314669aae79fd89d3ffe
jack <jackzhoumine@gmail.com>
2022-04-18 21:09:00 +0800

    feat: 集中管理组件

--- index.js
+++ index.js
@@ -2,46 +2,25 @@
  * @Description : koa demo1
  * @Date        : 2022-04-12 00:07:53 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-04-18 20:54:46 +0800
+ * @LastEditTime: 2022-04-18 21:08:17 +0800
  * @LastEditors : JackChou
  */
-const path = require('path')
 const Koa = require('koa')
 const app = new Koa()
-const bodyParer = require('koa-bodyparser')
-const nunjucks = require('koa-nunjucks-2')
+
 const router = require('./src/routers')
 const middlewares = require('./src/middlewares')
-// const { parsePostData } = require('./src/utils')
-const koaStatic = require('koa-static')
 
-//NOTE dist 目录下有 index.html，访问 /  会渲染这个文件
-app.use(koaStatic('./dist')) //NOTE 静态资源都放在 dist 文件夹下
-// 在 html 中这样引入 <link rel="stylesheet" href="/main.css" />
-
-// NOTE nunjucks 配置
-app.use(
-  nunjucks({
-    ext: 'html',
-    path: path.join(__dirname, './src/views'), // 指定视图目录
-    nunjucksConfig: {
-      trimBlocks: true, // 开启转义 防Xss
-    },
-  }),
-)
-
-app.use(bodyParer())
-app.use(middlewares.handleError)
-app.use(middlewares.sendJson)
-// console.log(middlewares.sendJson)
 app.on('error', (err, ctx) => {
   console.log({ path: ctx.path, method: ctx.method, info: err.message })
 })
 
+// 集中管理中间件
+middlewares(app)
+
 router(app)
 
 const PORT = process.env.PORT || 3000
-
 app.listen(PORT, () => {
   console.info(`server is running at port http://localhost:${PORT}`)
 })
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,19 +2,45 @@
  * @Description : 中间件出口文件
  * @Date        : 2022-04-18 19:49:30 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-04-18 20:47:27 +0800
+ * @LastEditTime: 2022-04-18 21:07:32 +0800
  * @LastEditors : JackChou
  */
+const path = require('path')
+const bodyParer = require('koa-bodyparser')
+const nunjucks = require('koa-nunjucks-2')
+// const { parsePostData } = require('./src/utils')
+const koaStatic = require('koa-static')
+//NOTE dist 目录下有 index.html，访问 /  会渲染这个文件
+// 在 html 中这样引入 <link rel="stylesheet" href="/main.css" />
+
 const sendJson = require('./sendJson')
-module.exports = {
-  async handleError(ctx, next) {
-    try {
-      await next()
-    } catch (error) {
-      ctx.status = 500
-      ctx.body = { path: ctx.path, info: error.message, method: ctx.method }
-      ctx.app.emit('error', error, ctx)
-    }
-  },
-  sendJson,
+
+async function handleError(ctx, next) {
+  try {
+    await next()
+  } catch (error) {
+    ctx.status = 500
+    ctx.body = { path: ctx.path, info: error.message, method: ctx.method }
+    ctx.app.emit('error', error, ctx)
+  }
+}
+
+module.exports = (app) => {
+  app.use(handleError)
+
+  app.use(bodyParer())
+  app.use(sendJson)
+
+  app.use(koaStatic('./dist')) //NOTE 静态资源都放在 dist 文件夹下
+
+  // NOTE nunjucks 配置
+  app.use(
+    nunjucks({
+      ext: 'html',
+      path: path.resolve(__dirname, '../views'), // 指定视图目录
+      nunjucksConfig: {
+        trimBlocks: true, // 开启转义 防Xss
+      },
+    }),
+  )
 }
