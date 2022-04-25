e4a986f5dd1958375a25c59902c4d46ee8481dee
jack <jackzhoumine@gmail.com>
2022-04-25 20:20:35 +0800

    feat: 设置服务器时间

--- src/middlewares/serverTime.js
+++ src/middlewares/serverTime.js
@@ -2,12 +2,12 @@
  * @Description : 记录服务器时间
  * @Date        : 2022-04-18 23:56:39 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-04-19 00:19:44 +0800
+ * @LastEditTime: 2022-04-25 20:19:37 +0800
  * @LastEditors : JackChou
  */
 module.exports = () => {
   return async (ctx, next) => {
     await next()
-    ctx.set('Server-Time', 1200)
+    ctx.set('Server-Timing', 1200)
   }
 }
